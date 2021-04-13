c9c0afe530672175c584bf71ef314ef2b7e2a331
AdamKorzun <a.korzun21@gmail.com>
2021-04-13 23:28:28 +0300

    lab6 update

--- sem4/task6.py
+++ sem4/task6.py
@@ -41,54 +41,67 @@ class Task6:
             y[i] = p[i] + ((-1) ** k) * m
         return y
 
+def test_1():
+    x_array = np.array([1, 2, 3])
+    y_array = np.array([1, 4, 9])
+    print("Lagrange polynomial: \n{}".format(Task6.lagrange_method(
+    x_array, y_array)))
+
+    print("Newton polynomial: \n{}".format(Task6.newton_method(x_array, y_array)))
+    print("Lagrange polynomial(1.5): {}".format(Task6.lagrange_method(
+    x_array, y_array)(1.5)))
+    print("Newton polynomial(1.5): {}".format(Task6.newton_method(x_array, y_array)(1.5)))
+    plt.plot(x_array, y_array, 'o', x_array, Task6.lagrange_method(
+    x_array, y_array)(x_array))
+    plt.grid(True)
+    plt.show()
+
+def test_2():
+    x_array = np.array([1, 2, 3, 4])
+    y_array = np.array([1, 8, 27, 64])
+    print("Lagrange polynomial: \n{}".format(Task6.lagrange_method(
+    x_array, y_array)))
+
+    print("Newton polynomial: \n{}".format(Task6.newton_method(x_array, y_array)))
+    print("Lagrange polynomial(2.31): {}".format(Task6.lagrange_method(
+    x_array, y_array)(2.31)))
+    print("Newton polynomial(2.31): {}".format(Task6.newton_method(x_array, y_array)(2.31)))
+    plt.plot(x_array, y_array, 'o', x_array, Task6.lagrange_method(
+    x_array, y_array)(x_array))
+    plt.grid(True)
+    plt.show()
+
+def test_3():
+    x_array = np.array([-1, 1, 2])
+    y_array = np.array([2, 3, -1])
+    print("Lagrange polynomial: \n{}".format(Task6.lagrange_method(
+    x_array, y_array)))
+
+    print("Newton polynomial: \n{}".format(Task6.newton_method(x_array, y_array)))
+    print("Lagrange polynomial(2.31): {}".format(Task6.lagrange_method(
+    x_array, y_array)(2.31)))
+    print("Newton polynomial(2.31): {}".format(Task6.newton_method(x_array, y_array)(2.31)))
+    plt.plot(x_array, y_array, 'o', x_array, Task6.lagrange_method(
+    x_array, y_array)(x_array))
+    plt.grid(True)
+    plt.show()
 
 if __name__ == '__main__':
+
     x_array = np.arange(0, 1.1, 0.1)
     y_array = Task6.get_array(x_array, 8, 4)
-
+    #x_array = np.array([i for i in range(0, 1.1, 0.1)])
 
     approximation = np.poly1d(np.polyfit(x_array, y_array, 10))
-    print(f"Интерполяционный многочлен Лагранжа: \n{Task6.lagrange_method(x_array, y_array)}")
-    print(f"Интерполяционный многочлен Ньютона: \n{Task6.newton_method(x_array, y_array)}")
-    print(f"Многочлен наилучшего приближения: \n{approximation}")
-    print(f"Значение интерполяционного многочлена в 0.47: {Task6.lagrange_method(x_array, y_array)(0.47)}")
-    print(f"Значение многочлена наилучшего приближения в 0.47: {approximation(0.47)}")
-    y_plot = Task6.lagrange_method(x_array, y_array)(x_array)
-    plt.plot(x_array, y_array, 'o', x_array, y_plot)
-    plt.grid(True)
-    plt.show()
-    '''
-    print("\nТестовый пример 1")
-    x = np.array([-1.5, -0.75, 0, 0.75, 1.5])
-    y = np.array([-14.1014, -0.931596, 0, 0.931596, 14.1014])
-    test_lagrange = Task6lagrange(x, y)
-    test_newton = newton(x, y)
-    approximation = np.poly1d(np.polyfit(x, y, 3))
-    print(f"Интерполяционный многочлен Лагранжа: \n{test_lagrange}")
-    print(f"Интерполяционный многочлен Ньютона: \n{test_newton}")
-    print(f"Многочлен наилучшего приближения: \n{approximation}")
-    print(f"Значение интерполяционного многочлена в 0.47: {test_lagrange(0.47)}")
-    print(f"Значение многочлена наилучшего приближения в 0.47: {approximation(0.47)}")
-    x_new = np.arange(-2, 2, 0.1)
-    y_plot = test_newton(x_new)
-    plt.plot(x, y, 'o', x_new, y_plot)
-    plt.grid(True)
-    plt.show()
+    print("Lagrange  polynomial: \n{}".format(Task6.lagrange_method(
+    x_array, y_array)))
+    print("Newton polynomial: \n{}".format(Task6.newton_method(x_array, y_array)))
+    print("Approximation: \n{}".format(approximation))
+    print("Lagrange polynomial(0.47): {}".format(Task6.lagrange_method(
+    x_array, y_array)(0.47)))
+    print("Newton polynomial(0.47): {}".format(Task6.newton_method(x_array, y_array)(0.47)))
+    plt.plot(x_array, y_array, 'o', x_array, Task6.lagrange_method(
+    x_array, y_array)(x_array))
 
-    print("\nТестовый пример 2")
-    x = np.array([-5.0, -1.0, 0.0, 2.0])
-    y = np.array([-2.0, 6.0, 1.0, 3.0])
-    test_lagrange2 = lagrange(x, y)
-    test_newton2 = newton(x, y)
-    approximation = np.poly1d(np.polyfit(x, y, 3))
-    print(f"Интерполяционный многочлен Лагранжа: \n{test_lagrange2}")
-    print(f"Интерполяционный многочлен Ньютона: \n{test_newton2}")
-    print(f"Многочлен наилучшего приближения: \n{approximation}")
-    print(f"Значение интерполяционного многочлена в 0.47: {test_lagrange2(0.47)}")
-    print(f"Значение многочлена наилучшего приближения в 0.47: {approximation(0.47)}")
-    x_new = np.arange(-5.1, 2.1, 0.1)
-    y_plot = test_newton2(x_new)
-    plt.plot(x, y, 'o', x_new, y_plot)
     plt.grid(True)
     plt.show()
-    '''
